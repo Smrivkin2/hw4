59b2d41434d0b73419467c22ac1b4baf230319e8
Smrivkin2 <stephanie.rivkin@kellogg.northwestern.edu>
2022-03-15 23:15:38 +0000

    Commit without Heroku

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,30 @@
 class PlacesController < ApplicationController
 
   def index
+    if @current_user
     @places = Place.all
-  end
-
-  def show
-    @place = Place.find(params["id"])
+    else
+     redirect_to "/sessions/new"     
+    end
   end
 
   def new
     @place = Place.new
   end
 
+  def show
+    if @current_user
+    @place = Place.find(params["id"])
+    # @post = Post.find(params["id"])
+    # @post.user_id = @current_user.id
+    end
+  end
+
   def create
+    if @current_user
     @place = Place.new(params["place"])
     @place.save
+    end
     redirect_to "/places"
   end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,18 @@ class PostsController < ApplicationController
     @post.place_id = params["place_id"]
   end
 
-  def create
+  def show 
     @post = Post.new(params["post"])
-    @post.save
-    redirect_to "/places/#{@post.place.id}"
   end
 
+  def create
+    if @current_user
+      @post = Post.new(params["post"])
+      @post.user_id = @current_user.id
+      @post.save
+      redirect_to "/places/#{@post.place.id}"
+    else
+      flash[:notice] = "Login first."
+    end
+    end
 end
